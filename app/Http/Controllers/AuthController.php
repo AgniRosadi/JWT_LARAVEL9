e01080a3a9cdea19c457c1822c21017ab13e6411
Agni Rosadi <agnirosadi08@gmail.com>
2022-06-23 16:08:34 +0700

    JWT

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+class AuthController extends Controller
+{
+    //
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        
+        //cek token
+        $token  = Auth::attempt($credentials);
+        if(!$token)
+        {
+            return response()->json([
+                'status' => 'error',
+                "message" => 'Unauthorized',
+                'error_code' => 401
+            ]);
+        }
+        
+        //cek user
+        $user = Auth::user();
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer'
+            ]
+        ]);
+    }
+
+
+    //Register
+    public function register(Request $request)
+    {
+        $users = DB::table('users')->get();
+        $tes = [];
+        $email = $request->email;
+        foreach ($users as $user)
+        {
+            $tes = $user->email;
+            // var_dump($user->name);
+        }
+        //validasi data mandatory
+        $cek = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:6',
+        ]);
+        
+        // print_r($tes);
+        // exit();
+        
+        if($email == $tes){
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Error, email already exists',
+                'error_code' => 401
+            ]);
+        }
+        
+        //query add user
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+
+        $token = Auth::login($user);
+        // var_dump($token);
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User created successfully',
+            'user' => $user,
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ]);
+    
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Successfully logged out'
+        ]);
+    }
+
+    public function refresh()
+    {
+        return response()->json([
+            'status' => 'success',
+            'user' => Auth::user(),
+            'authorisation' => [
+                'token' => Auth::refresh(),
+                'type' => 'bearer',
+            ]
+        ]);
+    }
+}
---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    //
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    public function index()
+    {
+        $todos = Todo::all();
+        return response()->json([
+            'status' => 'success',
+            'todos' => $todos,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
+        ]);
+
+        $todo = Todo::create([
+            'title' => $request->title,
+            'decription' => $request->description
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'todo' => $todo,
+        ]);
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
+        ]);
+        
+        $todo = Todo::find($id);
+        $todo->title = $request->title;
+        $todo->description = $request->description;
+        $todo->sava();
+
+
+        return response()->json([
+            'status' => 'success',
+            'messages' => 'Todo updated successfully',
+            'todo' => $todo
+
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        $todo = Todo::find($id);
+        $todo->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'messages' => 'Todo deleted successfully',
+            'todo' => $todo,
+        ]);
+    }
+}
